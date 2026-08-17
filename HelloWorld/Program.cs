43f1323777414b8b31c42d530c200aff0244e9db
Gutz1913 <andresfgutierrezv@gmail.com>
2026-08-17 17:46:00 -0500

    Add new code for age calculation

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,9 +4,36 @@ class Program
 {
     static void Main()
     {
-        var Name = "Andrés";
-        Name = "1Andrés";
-        Console.WriteLine($"Hello World! {Name}");
+        var dateConverted = new DateOnly();
+        var nameInput = string.Empty;
+        var birthDateInput = string.Empty;
+        Console.WriteLine("¡Hola, Bienvenido al calculador de años!");
+        Console.Write("Escribe tu nombre: ");
+        nameInput = Console.ReadLine();
+        Console.WriteLine($"Un gusto conocerte {nameInput}");
+        Console.Write("Escribe tu fecha de nacimiento en formato dd/mm/yy: ");
+        birthDateInput = Console.ReadLine();
+        bool isDateValid = DateOnly.TryParse(birthDateInput, out dateConverted);
+        if (!isDateValid)
+            Console.WriteLine($"La fecha de nacimiento es inválida, usted nos envió este dato erróneo: {birthDateInput}");
+        var person = new Person
+        {
+            Name = nameInput,
+            BirthDate = dateConverted,
+            Age = DateTime.Now.Year - dateConverted.Year
+        };
+
+        Console.WriteLine($"Tú nombre es.............: {person.Name}");
+        Console.WriteLine($"Tú fecha de nacimiento es: {person.BirthDate}");
+        Console.WriteLine($"Tú edad es...............: {person.Age} años");
+
         Console.WriteLine();
-    } 
+    }
+
+    public class Person
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public DateOnly BirthDate { get; set; }
+    }
 }
