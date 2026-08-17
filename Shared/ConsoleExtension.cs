ad76ec77697ecb85001b2d757510dc3ffdca8da8
Gutz1913 <andresfgutierrezv@gmail.com>
2026-08-17 18:14:43 -0500

    Add a new shared class to facilitate operations

---
+++ Shared/ConsoleExtension.cs
@@ -0,0 +1,68 @@
+﻿namespace Shared;
+
+public class ConsoleExtension
+{
+    public static int GetInt(string message)
+    {
+        Console.Write(message);
+        var input = Console.ReadLine();
+        if (int.TryParse(input, out int numberInt))
+        {
+            return numberInt;
+        }
+        throw new Exception("El valor ingresado no es válido.");
+    }
+
+    public static float GetFloat(string message)
+    {
+        Console.Write(message);
+        var input = Console.ReadLine();
+        if (float.TryParse(input, out float numberFloat))
+        {
+            return numberFloat;
+        }
+        throw new Exception("El valor ingresado no es válido.");
+    }
+
+    public static double GetDouble(string message)
+    {
+        Console.Write(message);
+        var input = Console.ReadLine();
+        if (double.TryParse(input, out double numberDouble))
+        {
+            return numberDouble;
+        }
+        throw new Exception("El valor ingresado no es válido.");
+    }
+
+    public static decimal GetDecimal(string message)
+    {
+        Console.Write(message);
+        var input = Console.ReadLine();
+        if (decimal.TryParse(input, out decimal numberDecimal))
+        {
+            return numberDecimal;
+        }
+        throw new Exception("El valor ingresado no es válido.");
+    }
+
+    public static string GetString(string message)
+    {
+        Console.Write(message);
+        var text = Console.ReadLine();
+        return text;
+
+        throw new Exception("El valor ingresado no es válido.");        
+    }
+
+    public static string? GetValidOptions(string message, List<string> options)
+    {
+        Console.Write(message);
+        var answer = Console.ReadLine();
+        if (options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)))
+        {
+            return answer;
+        }
+        return null;
+    }
+}
